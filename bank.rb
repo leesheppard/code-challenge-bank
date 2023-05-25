c6c7b526ec3108b2df21f9197d2311243d64e79b
Lee Sheppard <lee@leesheppard.com>
2023-05-25 23:25:30 +1000

    Raise exceptions for account balance issues

--- bank.rb
+++ bank.rb
@@ -1,22 +1,33 @@
+# frozen_string_literal: true
+
 require 'csv'
 
 class Bank
   def initialize
     @accounts = {}
   end
-  
+
   def load_accounts(csv_filename)
     CSV.foreach(csv_filename) do |row|
-      account_number, balance = row[0], row[1].to_f
-      
+      account_number = row[0]
+      balance = row[1].to_f
+      raise 'Invalid account number' if account_number.size != 16
+      raise 'Invalid balance value' if balance.negative?
+
       @accounts[account_number] = balance
     end
   end
 
   def process_transfers(csv_filename)
     CSV.foreach(csv_filename) do |row|
-      from_account, to_account, amount = row[0], row[1], row[2].to_f
-      
+      from_account = row[0]
+      to_account = row[1]
+      amount = row[2].to_f
+      raise 'Invalid account number' if from_account.size != 16 || to_account.size != 16
+      raise 'Invalid transfer amount' if amount.negative?
+      raise 'Account not found' unless @accounts.key?(from_account) && @accounts.key?(to_account)
+      raise 'Insufficient funds' if (@accounts[from_account] - amount).negative?
+
       @accounts[from_account] -= amount
       @accounts[to_account] += amount
     end
